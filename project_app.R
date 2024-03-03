1ede4009816d29dede9fae834bb54495c17b76c2
peysonsmith <114314207+peysonsmith@users.noreply.github.com>
2024-03-03 13:43:15 -0800

    Add files via upload

---
+++ project_app.R
@@ -0,0 +1,6 @@
+library(shiny)
+
+source("project_ui.R")
+source("project_server.R")
+
+shinyApp(ui = ui, server = server)
---
+++ project_ui.R
@@ -0,0 +1,81 @@
+
+## OVERVIEW TAB INFO
+
+overview_tab <- tabPanel("Overview Tab Title",
+   h1("Some title"),
+   p("some explanation")
+)
+
+## VIZ 1 TAB INFO
+
+viz_1_sidebar <- sidebarPanel(
+  h2("Options for graph"),
+  #TODO: Put inputs for modifying graph here
+)
+
+viz_1_main_panel <- mainPanel(
+  h2("Vizualization 1 Title"),
+  # plotlyOutput(outputId = "your_viz_1_output_id")
+)
+
+viz_1_tab <- tabPanel("Viz 1 tab title",
+  sidebarLayout(
+    viz_1_sidebar,
+    viz_1_main_panel
+  )
+)
+
+## VIZ 2 TAB INFO
+
+viz_2_sidebar <- sidebarPanel(
+  h2("Options for graph"),
+  #TODO: Put inputs for modifying graph here
+)
+
+viz_2_main_panel <- mainPanel(
+  h2("Vizualization 2 Title"),
+  # plotlyOutput(outputId = "your_viz_1_output_id")
+)
+
+viz_2_tab <- tabPanel("Viz 2 tab title",
+  sidebarLayout(
+    viz_2_sidebar,
+    viz_2_main_panel
+  )
+)
+
+## VIZ 3 TAB INFO
+
+viz_3_sidebar <- sidebarPanel(
+  h2("Options for graph"),
+  #TODO: Put inputs for modifying graph here
+)
+
+viz_3_main_panel <- mainPanel(
+  h2("Vizualization 3 Title"),
+  # plotlyOutput(outputId = "your_viz_1_output_id")
+)
+
+viz_3_tab <- tabPanel("Viz 3 tab title",
+  sidebarLayout(
+    viz_3_sidebar,
+    viz_3_main_panel
+  )
+)
+
+## CONCLUSIONS TAB INFO
+
+conclusion_tab <- tabPanel("Conclusion Tab Title",
+ h1("Some title"),
+ p("some conclusions")
+)
+
+
+
+ui <- navbarPage("Example Project Title",
+  overview_tab,
+  viz_1_tab,
+  viz_2_tab,
+  viz_3_tab,
+  conclusion_tab
+)
